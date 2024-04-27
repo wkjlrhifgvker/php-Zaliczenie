6a20159ea2e4abd3b5e45b4f8efc2bb74b0bf118
lokoloko131345 <danielzagroda@gmail.com>
2024-04-27 11:56:47 +0200

    Add types of  notes

--- index.php
+++ index.php
@@ -5,8 +5,38 @@ namespace App;
 
 include_once('./src/utils/debug.php');
 
-dump('test');
-dump('====dump====')
-
-
-
+if(!empty($_GET['action'])){
+    $action = $_GET['action'];
+}else{
+    $action = null;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Moje notatki</h1>
+    </header>
+    <main>
+        <nav>
+            <ul>
+                <li><a href="/">Lista notatek</a></li>
+                <li><a href="/?action=create">Nowa notatka</a></li>
+            </ul>
+        </nav>
+        <article>
+            <?php if ($action === 'create') : ?>
+                <h3>Nowa notatka</h3>
+                <?php else : ?>
+                    <h3>Lista notatek</h3>
+                    <?php endif; ?>
+        </article>
+    </main>
+    <footer>Stopka</footer>
+</body>
+</html>
