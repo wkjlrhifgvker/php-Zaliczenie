94e11dce61ef84c99f8b46c3472866f2578a4d96
lokoloko131345 <danielzagroda@gmail.com>
2024-04-28 10:45:27 +0200

    restruction controller

--- index.php
+++ index.php
@@ -6,32 +6,10 @@ namespace App;
 require_once('./src/controller.php');
 include_once('./src/utils/debug.php');
 
-// const DEAFULT_ACTION = 'list';
 
-// $action = $_GET['action'] ?? DEAFULT_ACTION;
-
-// $viewParams = [];
-
-// switch($action){
-//     case 'create':
-//         $page = 'create';
-//         $created = false;
-//         if(!empty($_POST)){
-//                 $viewParams = [
-//                     'title' => $_POST['title'],
-//                     'description' => $_POST['description'],
-//                 ];
-//                 $created = true;
-//                         }
-//             $viewParams['created'] = $created;
-//             break;
-//             default:
-//             $page = 'list';
-//             $viewParams['resultList'] = 'Wyświetlamy listę notatek';
-//     }
-
-
-
-// $view->render($page, $viewParams);
-$controller = new Controller($_GET, $_POST);
+$request = [
+    'get' => $_GET,
+    'post' => $_POST,
+];
+$controller = new Controller($request);
 $controller->run();
--- src/controller.php
+++ src/controller.php
@@ -6,45 +6,56 @@ namespace App;
 
 include('./src/View.php');
 
-
  class Controller
  {
-        const DEFAULT_ACTION = 'list';
-        private array $getData;
-        private array $postData;
+    const DEFAULT_ACTION = 'list';
+    private View $view;
+    private array $request;
 
-public function __construct(array $getData, array $postData)
-{
-    $this->getData = $getData;
-    $this->postData = $postData;
-}
-public function run(): void 
-{
-$action = $this->getData['action'] ?? self::DEFAULT_ACTION;
-$view = new View();
-
-$viewParams = [];
-
-switch($action){
-    case 'create':
-        $page = 'create';
-        $created = false;
-        if(!empty($this->postData)){
-                $viewParams = [
-                    'title' => $this->postData['title'],
-                    'description' => $this->postData['description'],
-                ];
-                $created = true;
-                        }
-            $viewParams['created'] = $created;
-            break;
-            default:
-            $page = 'list';
-            $viewParams['resultList'] = 'Wyświetlamy listę notatek';
+    public function __construct(array $request)
+    {
+        $this->request = $request;
+        $this->view = new View();
     }
+    public function run(): void 
+    {
+
+    $viewParams = [];
+
+    switch($this->action()){
+        case 'create':
+            $page = 'create';
+            $created = false;
+            $data = $this->getRequestPost();
+            if(!empty($data)){
+                    $viewParams = [
+                        'title' => $data['title'],
+                        'description' => $data['description'],
+                    ];
+                    $created = true;
+                            }
+                $viewParams['created'] = $created;
+                break;
+                default:
+                $page = 'list';
+                $viewParams['resultList'] = 'Wyświetlamy listę notatek';
+        }
 
 
 
-$view->render($page, $viewParams);
+    $this->view->render($page, $viewParams);
+        }
+        private function action(): string 
+        {
+            $data = $this->getRequestGet();
+            return $data['action'] ?? self::DEFAULT_ACTION;
+        }
+            private function getRequestGet(): array 
+            {
+                return $this->request['get'] ?? [];
+            }
+            private function getRequestPost(): array 
+            {
+                return $this->request['post'] ?? [];
+            }
     }
-}
