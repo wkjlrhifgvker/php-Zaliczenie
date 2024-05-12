ad57e3bedab48be4d9aa8ca3ea7a4bd7132c0382
lokoloko131345 <danielzagroda@gmail.com>
2024-05-12 13:00:33 +0200

    automatyzacja zabezpeczenie htment

--- src/View.php
+++ src/View.php
@@ -2,10 +2,26 @@
   declare(strict_types=1);
 
   namespace App;
-   class View
-   {
+class View
+{
     public function render(?string $page, array $params = []): void 
     {
+        $params = $this->escape($params);
         require_once('./templates/layout.php');
     } 
+    private function escape (array $params): array 
+    {
+    $clearParams =[];
+
+    foreach ($params as $key => $param) {
+      if (is_array($param)) {
+        $clearParams[$key] = $this->escape($param);
+      } else if ($param) {
+        $clearParams[$key] = htmlentities($param);
+    }else {
+      $clearParams[$key] = $param;
    }
+  }
+  return $clearParams;
+ }
+}
